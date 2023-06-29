dd6bf21be896d2b70786a558b3eb0ed47ddb205a
Edwin Ariza <me@edwinariza.com>
2023-06-29 06:32:00 -0500

    VERSION: ARITHMETIC SUPPORT.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pjson
 Methods to transform json files, maths, filters and more
+
+# INSTALL 
+    composer require jumichica/pjson
+
+# Examples
+# Arithmetic Procesing
+If you define a atribute with an initial equal (=) the system analyze te content to eval the formula.
+
+    $omath = new Math();
+    $json = '[
+    {"A": 1,"B": 2, "C": "= A + B"},
+    {"A": 1, "B": 2, "C": "= A - B"},
+    {"A": 1, "B": 2,"C": "= A * B"},
+    {"A": 1,"B": 2,"C": "= A / B"}
+    ]';
+    $pjson = $omath->arithmetic($json);
+    print_r($pjson);
+
---
+++ src/math/main.php
@@ -0,0 +1,59 @@
+<?php
+/***
+ * Math operations over json.
+ * @author Edwin Ariza <me@edwinariza.com>
+ * @copyright Jumichica
+ * @license MIT
+ */
+class Math{
+    /**
+     * @param $json Json string to transform
+     * @return String
+     */
+    public function arithmetic($json){
+        $pjson="";
+        // ENCODE JSON STRING
+        $json = json_decode($json);
+        // PROCESS THE JSON
+
+        foreach ($json as $rkey => $row) {
+            foreach ($row as $key => $value){
+                $formula = "";
+                $first_character = substr($value, 0, 1);
+                $iresult = 0;
+                $bformula="";
+                if($first_character=='='){
+                    $bformula = $value;
+                    $formula = str_replace("=", "", $value);
+                    $formula = str_replace(" ","", $formula);
+                    foreach ($row as $ikey => $ivalue){
+                        $formula = str_replace(strtoupper($ikey), $ivalue, $formula);
+                    }
+
+                    $formula = '$iresult = '.$formula.';';
+                    // EVAL THE FORMULE
+                    eval($formula);
+                    $json[$rkey]->$key = "{'formula': '$bformula', 'value':'$iresult'}";
+                }
+                else{
+                    $iresult = $value;
+                    $json[$rkey]->$key = $iresult;
+                }
+            }
+        }
+        $json = json_encode($json);
+        return $json;
+    }
+}
+
+/***
+$omath = new Math();
+$json = '[
+            {"A": 1,"B": 2, "C": "= A + B"},
+            {"A": 1, "B": 2, "C": "= A - B"},
+            {"A": 1, "B": 2,"C": "= A * B"},
+            {"A": 1,"B": 2,"C": "= A / B"}
+        ]';
+$pjson = $omath->arithmetic($json);
+print_r($pjson);
+***/
